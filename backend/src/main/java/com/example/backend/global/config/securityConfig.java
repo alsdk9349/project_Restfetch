1db570b3da79aaaf524e39181de1463d18e8a67d
김민아 <alsdk9349@naver.com>
2024-10-02 15:37:18 +0900

    fix:redis-ip-modify4

--- backend/src/main/java/com/example/backend/global/config/securityConfig.java
+++ backend/src/main/java/com/example/backend/global/config/securityConfig.java
@@ -88,8 +88,8 @@ public class securityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(Arrays.asList("https://localhost:3000","http://localhost:3000", "https://client:3000","http://client:3000", "http://j11c209.p.ssafy.io", "http://j11c209.p.ssafy.io:8080"));
-//        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+//        config.setAllowedOrigins(Arrays.asList("https://localhost:3000","http://localhost:3000", "https://client:3000","http://client:3000", "https://i11c208.p.ssafy.io", "http://i11c208.p.ssafy.io"));
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
         // todo : 여기에 허용할 헤더 목록
         config.setAllowedHeaders(Arrays.asList("*"));
