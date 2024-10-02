334bab32b3af4bc6e4874cceee24b48d82398bb5
ImSeongHun <daltjdgns111@naver.com>
2024-10-02 10:39:48 +0900

    fix : 응답 코드 수정

--- backend/src/main/java/com/example/backend/global/result/ResultCode.java
+++ backend/src/main/java/com/example/backend/global/result/ResultCode.java
@@ -10,24 +10,24 @@ import org.springframework.http.HttpStatus;
 public enum ResultCode {
 
     // 기본형
-    COMMON_OK(HttpStatus.OK, "C001", "요청을 처리했습니다."),
+    COMMON_OK(HttpStatus.OK, "S000", "요청을 처리했습니다."),
 
     // Member
-    LOGIN_OK(HttpStatus.OK, "M001", "로그인 했습니다."),
-    SIGNUP_OK(HttpStatus.CREATED, "M002", "회원가입에 성공했습니다."),
-    EMAIL_SEND_OK(HttpStatus.ACCEPTED, "M003", "이메일 전송에 성공했습니다."),
-    VALIDATION_NUMBER_OK(HttpStatus.OK, "M004", "이메일 인증이 성공했습니다."),
-    LOGOUT_OK(HttpStatus.OK , "M005", "로그아웃 했습니다."),
+    LOGIN_OK(HttpStatus.OK, "S001", "로그인 했습니다."),
+    SIGNUP_OK(HttpStatus.CREATED, "S002", "회원가입에 성공했습니다."),
+    EMAIL_SEND_OK(HttpStatus.ACCEPTED, "S003", "이메일 전송에 성공했습니다."),
+    VALIDATION_NUMBER_OK(HttpStatus.OK, "S004", "이메일 인증이 성공했습니다."),
+    LOGOUT_OK(HttpStatus.OK , "S005", "로그아웃 했습니다."),
 
     // Fetch
-    FETCH_REGISTER_OK(HttpStatus.OK, "F001", "패치 등록 성공했습니다."),
-    FETCH_DELETE_OK(HttpStatus.OK, "F002", "패치 삭제 성공했습니다."),
-    SEARCH_FETCH_OK(HttpStatus.OK, "F003", "패치 조회 성공했습니다."),
+    FETCH_REGISTER_OK(HttpStatus.OK, "S006", "패치 등록 성공했습니다."),
+    FETCH_DELETE_OK(HttpStatus.OK, "S007", "패치 삭제 성공했습니다."),
+    SEARCH_FETCH_OK(HttpStatus.OK, "S008", "패치 조회 성공했습니다."),
 
     // Observer
-    OBSERVER_REGISTER_OK(HttpStatus.OK, "O001", "옵저버 등록 성공했습니다."),
-    OBSERVER_DELETE_OK(HttpStatus.OK, "O002", "옵저버 삭제 성공했습니다."),
-    SEARCH_OBSERVER_OK(HttpStatus.OK, "O003", "옵저버 조회 성공했습니다.");
+    OBSERVER_REGISTER_OK(HttpStatus.OK, "S009", "옵저버 등록 성공했습니다."),
+    OBSERVER_DELETE_OK(HttpStatus.OK, "S010", "옵저버 삭제 성공했습니다."),
+    SEARCH_OBSERVER_OK(HttpStatus.OK, "S011", "옵저버 조회 성공했습니다.");
 
 
     private final HttpStatus status;
