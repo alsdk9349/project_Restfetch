a86ba6f7bace61776a50e1996ecd4f9b337fe9aa
김민아 <alsdk9349@naver.com>
2024-10-04 17:25:57 +0900

    feat:pick-start

---
+++ backend/src/main/java/com/example/backend/domain/pick/dto/PickRequestDto.java
@@ -0,0 +1,4 @@
+package com.example.backend.domain.pick.dto;
+
+public class PickRequestDto {
+}
---
+++ backend/src/main/java/com/example/backend/domain/pick/repository/PickRepository.java
@@ -0,0 +1,4 @@
+package com.example.backend.domain.pick.repository;
+
+public class PickRepository {
+}
