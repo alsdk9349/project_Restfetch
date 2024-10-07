590345bc782f3dc4744792c5f9d1e90a88cd66fd
ImSeongHun <daltjdgns111@naver.com>
2024-10-07 17:45:55 +0900

    feat : sse 알림

--- backend/src/main/java/com/example/backend/domain/Sse/service/SseServiceImpl.java
+++ backend/src/main/java/com/example/backend/domain/Sse/service/SseServiceImpl.java
@@ -1,35 +1,64 @@
 package com.example.backend.domain.Sse.service;
 
+import com.example.backend.domain.Sse.repository.SseRepository;
 import com.example.backend.domain.report.dto.response.ReportGetResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class SseServiceImpl implements SseService {
 
-    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
-    public SseEmitter subscribe(String id) {
+
+    private final SseRepository sseRepository;
+
+    public SseEmitter connect(String email) {
         SseEmitter emitter = new SseEmitter();
-        emitters.put(id, emitter);
 
-        emitter.onCompletion(() -> emitters.remove(id));
-        emitter.onTimeout(() -> emitters.remove(id));
+        sseRepository.save(email, emitter);
+
+        List<Object> cachedEvents = sseRepository.getAllEvents();
+        for (Object event : cachedEvents) {
+            try {
+                emitter.send(event); // 캐시된 이벤트 전송
+            } catch (Exception e) {
+                emitter.completeWithError(e); // 에러 처리
+
+            }
+        }
+
+        emitter.onCompletion(() -> sseRepository.remove(email));
+        emitter.onTimeout(() -> sseRepository.remove(email));
+
+        // 첫 연결 시 503에러 방지 위해 더미 데이터 전송
+        ReportGetResponseDto data = new ReportGetResponseDto();
+        send(data);
+
 
         return emitter;
     }
 
-    public void broadcast(ReportGetResponseDto reportData) {
-        emitters.forEach((sseId, emitter) -> {
+    public void send(ReportGetResponseDto reportData) {
+        log.info("Sending report to Sse");
+
+        sseRepository.getAll().forEach((key, emitter) -> {
             try {
+                log.info("key: {}, emitter: {}", key, emitter);
                 emitter.send(reportData); // 데이터 전송
             } catch (Exception e) {
+                log.info("fail");
                 emitter.completeWithError(e); // 에러 처리
-                emitters.remove(sseId); // 구독 취소
+                sseRepository.remove(key); // 구독 취소
+
+                sseRepository.cacheEvent(key, reportData);
             }
         });
     }
